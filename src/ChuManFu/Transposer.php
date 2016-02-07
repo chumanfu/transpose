8d9402cceb22a0718b62b0d5c721702611cfc938
root <root@192.168.10.1>
2016-02-07 23:02:44 +0000

    Added more tests and fixed the dominant notes in the scale.

--- src/ChuManFu/Transposer.php
+++ src/ChuManFu/Transposer.php
@@ -14,9 +14,9 @@ class Transposer
         'F',
         array('F#', 'Gb'),
         'G',
-        array('G#', 'Ab'),
+        array('Ab', 'G#'),
         'A',
-        array('A#', 'Bb'),
+        array('Bb', 'A#'),
         'B');
     private $search = '`([ABCDEFG][b#]?(?=\s?(?![a-zH-Z])|(?=(2|5|6|7|9|11|13|6\/9|7\-5|7\-9|7\#5|7\#9|7‌​\+5|7\+9|7b5|7b9|7sus2|7sus4|add2|add4|add9|aug|dim|dim7|m\|maj7|m6|m7|m7b5|m9|m1‌​1|m13|maj7|maj9|maj11|maj13|mb5|m|sus|sus2|sus4|\))(?=(\s|\/)))|(?=(\/|\.|-|\(|\)))))`';
     private $song;
--- test/TransposerTest.php
+++ test/TransposerTest.php
@@ -24,7 +24,8 @@ class TransposerTest extends PHPUnit_Framework_TestCase
      */
     public function testSimpleTranspose($newKey, $notes)
     {
-        $songText = "A B C D E F G";
+//        $songText = "A B C D E F G";
+        $songText = "C D E F G A B";
         $inKey = "C";
         
         $transposer = new ChuManFu\Transposer($songText);
@@ -36,13 +37,18 @@ class TransposerTest extends PHPUnit_Framework_TestCase
     public function inputAllSimpleScales()
     {
         $scales = array(); 
-        $scales[] = array('A', 'F# G# A B C# D E');
-        $scales[] = array('B', 'G# A# B C# Eb E F#');
-        $scales[] = array('C', 'A B C D E F G');
-        $scales[] = array('D', 'B C# D E F# G A');
-        $scales[] = array('E', 'C# Eb E F# G# A B');
-        $scales[] = array('F', 'D E F G A A# C');
-        $scales[] = array('G', 'E F# G A B C D');
+        $scales[] = array('A', 'A B C# D E F# Ab');
+        $scales[] = array('Bb', 'Bb C D Eb F G A');
+        $scales[] = array('B', 'B C# Eb E F# Ab Bb');
+        $scales[] = array('C', 'C D E F G A B');
+        $scales[] = array('C#', 'C# Eb F F# Ab Bb C');
+        $scales[] = array('D', 'D E F# G A B C#');
+        $scales[] = array('Eb', 'Eb F G Ab Bb C D');
+        $scales[] = array('E', 'E F# Ab A B C# Eb');
+        $scales[] = array('F', 'F G A Bb C D E');
+        $scales[] = array('F#', 'F# Ab Bb B C# Eb F');
+        $scales[] = array('G', 'G A B C D E F#');
+        $scales[] = array('Ab', 'Ab Bb C C# Eb F G');
         
         return $scales;
     }
